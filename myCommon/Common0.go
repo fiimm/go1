aadd6ed6133b40a762f40d8232d4487dc0771546
fiimm <fiim@ya.ru>
2022-01-18 20:10:53 +0300

    Update Common0.go

--- myCommon/Common0.go
+++ myCommon/Common0.go
@@ -1,4 +1,30 @@
 package myCommon
+import (
+    // "encoding/json"
+    // "flag"
+    // "fmt"
+    // "html/template"
+    // "log"
+    // "math"
+    "net/http"
+    // "net/url"
+    "os"
+    // "strconv"
+    // "time"
+)
+func common_main_port(portNN string)string{
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = portNN
+    }
+    return port
+}
+func common_main_static(staticFolder string){
+	s = "/" + staticFolder + "/"
+	mux := http.NewServeMux()
+    mux.Handle(s, http.StripPrefix(s, http.FileServer(http.Dir(staticFolder))))
+    return mux
+}
 func mySum(a, b int64)int64{
 	return a+b
 }
